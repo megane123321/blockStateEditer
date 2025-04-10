faf01da838eee48e7e591083b703bf44193e8de9
すりガラス眼鏡 <138665025+megane123321@users.noreply.github.com>
2025-04-10 12:46:52 +0900

    オーバーレイをBottonからColorRectに変更

--- over_ray.gd
+++ over_ray.gd
@@ -1,10 +1,25 @@
 class_name OverRay
-extends Button
+extends ColorRect
+
+var ray:Ray
 
 func _ready() -> void:
-	pressed.connect(disVisible)
 	set_size(get_viewport_rect().size)
 	set_position(Vector2(0,0))
+	color=Color(0,0,0,0.5)
+	ray=Ray.new()
+	add_child(ray)
 
 func disVisible() -> void:
-	visible=false
+		get_parent().remove_child(self)
+
+class Ray:
+	extends BaseButton
+
+	func _ready() -> void:
+		pressed.connect(disVisible)
+		set_size(get_viewport_rect().size)
+		set_position(Vector2(0,0))
+
+	func disVisible() -> void:
+		get_parent().disVisible()
