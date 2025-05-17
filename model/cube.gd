8ee5eb5dd26291eb353c336803fe1dcc15662bf8
すりガラス眼鏡 <138665025+megane123321@users.noreply.github.com>
2025-05-17 23:15:40 +0900

    頂点データ用に変更

--- model/cube.gd
+++ model/cube.gd
@@ -39,85 +39,85 @@ func _init(cubeInfo:Dictionary,workPath:String,blockRotateX:float=0,blockRotateY
 		point=rotate(point,Constant.BLOCK_CENTER,Vector3(0,-1,0),blockRotateY)
 		cube.append(point)
 	print(cubeInfo)
-	make_mesh(cube,cubeInfo,workPath)
+	#make_mesh(cube,cubeInfo,workPath)
 
-func move(vec:Vector3):
-	for i in len(cube):
-		cube[i]+=vec
+#func move(vec:Vector3):
+	#for i in len(cube):
+		#cube[i]+=vec
 
-func make_mesh(cubePoint:Array,cubeInfo:Dictionary,workPath:String):
-	var faces:Array
-	for key in [Constant.UP,Constant.DOWN,Constant.NORTH,Constant.EAST,Constant.WEST,Constant.SOUTH]:
-		if not cubeInfo[Constant.CUBE_FACES].has(key):
-			continue
-		var face:Array
-		match key:
-			Constant.UP:
-				face=[cubePoint[2],cubePoint[3],cubePoint[7],cubePoint[6]]
-			Constant.DOWN:
-				face=[cubePoint[4],cubePoint[5],cubePoint[1],cubePoint[0]]
-			Constant.NORTH:
-				face=[cubePoint[3],cubePoint[2],cubePoint[0],cubePoint[1]]
-			Constant.EAST:
-				face=[cubePoint[7],cubePoint[3],cubePoint[1],cubePoint[5]]
-			Constant.WEST:
-				face=[cubePoint[2],cubePoint[6],cubePoint[4],cubePoint[0]]
-			Constant.SOUTH:
-				face=[cubePoint[6],cubePoint[7],cubePoint[5],cubePoint[4]]
-		var edge1 = face[1] - face[0]
-		var edge2 = face[2] - face[0]
-		var normal = edge2.cross(edge1).normalized()
-		faces=[
-			[face[0],face[1],face[2],normal],
-			[face[0],face[2],face[3],normal]
-		]
-		var square:Array
-		if cubeInfo[Constant.CUBE_FACES][key].has(Constant.UV):
-			square=gen_square(cubeInfo[Constant.CUBE_FACES][key][Constant.UV])
-		else:
-			square=gen_square()
-		var uv_vertex:Array=[[square[0],square[1],square[2]],[square[0],square[2],square[3]]]
-		if cubeInfo.has([Constant.CUBE_ROTATION]):
-			pass
-			#match cubeInfo[Constant.CUBE_ROTATION]
-		for i in range(2):
-			var st = SurfaceTool.new()
-			st.begin(Mesh.PRIMITIVE_TRIANGLES)
-			st.set_normal(faces[i][3])
-			var material = StandardMaterial3D.new()
-			material.albedo_texture = TextureDataBase.getTexture(cubeInfo[Constant.CUBE_FACES][key][Constant.CUBE_TEXTURE],workPath)
-			material.transparency = BaseMaterial3D.TRANSPARENCY_ALPHA_SCISSOR
-			material.texture_filter = BaseMaterial3D.TEXTURE_FILTER_NEAREST
-			st.set_material(material)
-			for j in range(3):
-				st.set_uv(uv_vertex[i][j])
-				st.add_vertex(faces[i][j])#頂点指定
-			var mesh_instance = MeshInstance3D.new()
-			mesh_instance.mesh = st.commit()
-			add_child(mesh_instance)
-	## Commit to a mesh.
-	#var mesh = st.commit()
-	#var mesh_instance = MeshInstance3D.new()
-	#mesh_instance.mesh = mesh
-	#add_child(mesh_instance)
-
-func gen_square(corner:Array=[])->Array:
-	var square:Array
-	if corner==[]:
-		square=[
-			Vector2(0,0),
-			Vector2(1,0),
-			Vector2(1,1),
-			Vector2(0,1)
-		]
-	else:
-		square=[
-			Vector2(corner[0],corner[1])/Constant.BLOCK_PIXEL,
-			Vector2(corner[2],corner[1])/Constant.BLOCK_PIXEL,
-			Vector2(corner[2],corner[3])/Constant.BLOCK_PIXEL,
-			Vector2(corner[0],corner[3])/Constant.BLOCK_PIXEL
-		]
-	return square
+#func make_mesh(cubePoint:Array,cubeInfo:Dictionary,workPath:String):
+	#var faces:Array
+	#for key in [Constant.UP,Constant.DOWN,Constant.NORTH,Constant.EAST,Constant.WEST,Constant.SOUTH]:
+		#if not cubeInfo[Constant.CUBE_FACES].has(key):
+			#continue
+		#var face:Array
+		#match key:
+			#Constant.UP:
+				#face=[cubePoint[2],cubePoint[3],cubePoint[7],cubePoint[6]]
+			#Constant.DOWN:
+				#face=[cubePoint[4],cubePoint[5],cubePoint[1],cubePoint[0]]
+			#Constant.NORTH:
+				#face=[cubePoint[3],cubePoint[2],cubePoint[0],cubePoint[1]]
+			#Constant.EAST:
+				#face=[cubePoint[7],cubePoint[3],cubePoint[1],cubePoint[5]]
+			#Constant.WEST:
+				#face=[cubePoint[2],cubePoint[6],cubePoint[4],cubePoint[0]]
+			#Constant.SOUTH:
+				#face=[cubePoint[6],cubePoint[7],cubePoint[5],cubePoint[4]]
+		#var edge1 = face[1] - face[0]
+		#var edge2 = face[2] - face[0]
+		#var normal = edge2.cross(edge1).normalized()
+		#faces=[
+			#[face[0],face[1],face[2],normal],
+			#[face[0],face[2],face[3],normal]
+		#]
+		#var square:Array
+		#if cubeInfo[Constant.CUBE_FACES][key].has(Constant.UV):
+			#square=gen_square(cubeInfo[Constant.CUBE_FACES][key][Constant.UV])
+		#else:
+			#square=gen_square()
+		#var uv_vertex:Array=[[square[0],square[1],square[2]],[square[0],square[2],square[3]]]
+		#if cubeInfo.has([Constant.CUBE_ROTATION]):
+			#pass
+			##match cubeInfo[Constant.CUBE_ROTATION]
+		#for i in range(2):
+			#var st = SurfaceTool.new()
+			#st.begin(Mesh.PRIMITIVE_TRIANGLES)
+			#st.set_normal(faces[i][3])
+			#var material = StandardMaterial3D.new()
+			#material.albedo_texture = TextureDataBase.getTexture(cubeInfo[Constant.CUBE_FACES][key][Constant.CUBE_TEXTURE],workPath)
+			#material.transparency = BaseMaterial3D.TRANSPARENCY_ALPHA_SCISSOR
+			#material.texture_filter = BaseMaterial3D.TEXTURE_FILTER_NEAREST
+			#st.set_material(material)
+			#for j in range(3):
+				#st.set_uv(uv_vertex[i][j])
+				#st.add_vertex(faces[i][j])#頂点指定
+			#var mesh_instance = MeshInstance3D.new()
+			#mesh_instance.mesh = st.commit()
+			#add_child(mesh_instance)
+	### Commit to a mesh.
+	##var mesh = st.commit()
+	##var mesh_instance = MeshInstance3D.new()
+	##mesh_instance.mesh = mesh
+	##add_child(mesh_instance)
+#
+#func gen_square(corner:Array=[])->Array:
+	#var square:Array
+	#if corner==[]:
+		#square=[
+			#Vector2(0,0),
+			#Vector2(1,0),
+			#Vector2(1,1),
+			#Vector2(0,1)
+		#]
+	#else:
+		#square=[
+			#Vector2(corner[0],corner[1])/Constant.BLOCK_PIXEL,
+			#Vector2(corner[2],corner[1])/Constant.BLOCK_PIXEL,
+			#Vector2(corner[2],corner[3])/Constant.BLOCK_PIXEL,
+			#Vector2(corner[0],corner[3])/Constant.BLOCK_PIXEL
+		#]
+	#return square
 
 func rotate(point:Vector3,center:Vector3,axis:Vector3,angle:float)->Vector3:
 	var tmp:Vector3=point-center
--- model/model.gd
+++ model/model.gd
@@ -6,6 +6,5 @@ var cubes:Array
 func _init(modelJson:Dictionary,workPath:String,blockRotateX:float=0,blockRotateY:float=0) -> void:
 	if Constant.MODEL_CUBES in modelJson:
 		for tmp:Dictionary in modelJson[Constant.MODEL_CUBES]:
-			add_child(Cube.new(tmp,workPath,blockRotateX,blockRotateY))
-			#cubes.append(Cube.new(tmp,workPath))
+			cubes.append(Cube.new(tmp,workPath,blockRotateX,blockRotateY))
 	pass
